eb4e68c23cdcfdbe778bac5f94056bcd4aeb282a
Katrine <katrine.mie.thomsen@gmail.com>
2025-03-08 20:42:41 +0100

    nU TESTER VI OG TRÆNER VI

--- split_dataset.py
+++ split_dataset.py
@@ -12,26 +12,26 @@ train_cropped_dir = "King Domino dataset/train/cropped"
 test_full_dir = "King Domino dataset/test/full"
 test_cropped_dir = "King Domino dataset/test/cropped"
 
-# Opret mapperne hvis de ikke findes
+#mapperne hvis de ikke findes
 for folder in [train_full_dir, train_cropped_dir, test_full_dir, test_cropped_dir]:
     os.makedirs(folder, exist_ok=True)
 
-# Funktion til at opdele billeder
+#FSplitter  billeder, 
 def split_images(source_dir, train_dir, test_dir, split_ratio=0.8):
     images = os.listdir(source_dir)
-    random.shuffle(images)  # Bland billederne tilfældigt
+    random.shuffle(images)  #BlandERR billederne tilfældigt
 
     train_size = int(len(images) * split_ratio)
     train_images = images[:train_size]
     test_images = images[train_size:]
 
-    # Flyt billeder til de rigtige mapper
+    #Yeeter  billeder til de rigtige mapper HSAHA
     for img in train_images:
         shutil.copy(os.path.join(source_dir, img), os.path.join(train_dir, img))
     for img in test_images:
         shutil.copy(os.path.join(source_dir, img), os.path.join(test_dir, img))
 
-# Opdel billederne
+#SPLIT DE BITCHES OP (billederne)
 split_images(original_full_dir, train_full_dir, test_full_dir)
 split_images(original_cropped_dir, train_cropped_dir, test_cropped_dir)
 
